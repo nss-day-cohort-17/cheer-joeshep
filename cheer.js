2d836b8a4699d1926a8c55dc8e7c6dad3987f984
Joe Shepherd <joeshepmedia@gmail.com>
2017-03-01 14:09:15 -0600

    Add cheer exercise

---
+++ cheer.js
@@ -0,0 +1,19 @@
+console.log("Hello, world!");
+
+// Cheer
+const first = 'Joe'; //const
+const second = 'Shep'; //const
+
+// object literal shorthand
+const myName = { first, second } //const
+                              
+const nodeCheer = ({first, second}) => { // destructuring assignment
+  let name = `${first} ${second}`.toUpperCase(); // string template literals, let
+  [...first, ...second].forEach( (letter) => { // spread operator and fat arrow function
+    let conj = 'aeioufhlmnrsx'.includes(letter.toLowerCase()) ? 'an' : 'a' // `includes()`. Surprise! It's ES6
+    console.log(`Gimmmie ${conj} ${letter.toUpperCase()}!`); // string template literals again
+  });
+  console.log(`What's that spell?\n${name}!`);
+};
+
+nodeCheer(myName);
